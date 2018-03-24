a8973549664ad3d389835c262ba723fa4c742ceb
Dio_V <dio.ye@dadaabc.com>
2018-03-24 22:41:36 +0800

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Colors {
+  static const Color lightGreen = const Color(0xFFC8E3DA);
+  static const Color greyGreen = const Color(0xFFAAD4D4);
+  static const Color pictonBlue = const Color(0xFF4EC6FF);
+  static const Color waterMelon = const Color(0xFFFF5475);
+}
